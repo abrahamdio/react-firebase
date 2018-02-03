fcb8cf391ca67699ee4154e61fa2eb4499b6b806
dio <abrahamdio@gmail.com>
2018-02-03 01:15:06 -0800

    spacing, quotation marks

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import Expo from 'expo'
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 import allReducers from './src/reducers/index.js';
 import HomeScreen from './src/components/HomeScreen/index.js'
 import thunk from 'redux-thunk'
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 const store = createStore(allReducers, applyMiddleware(thunk));
 
 export default class NativeBaseRedux extends Component {
@@ -23,9 +18,9 @@ export default class NativeBaseRedux extends Component {
 
   async componentWillMount() {
     await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("native-base/Fonts/Ionicons.ttf")
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      Ionicons: require('native-base/Fonts/Ionicons.ttf')
     });
     this.setState({ isReady: true });
   }
